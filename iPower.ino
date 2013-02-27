7c09840b18bd5b504838ead609bc0a4e23fdf8d3
Alex <alex.agency@gmail.com>
2013-02-27 09:13:37 +0200

    update

--- iPower.ino
+++ iPower.ino
@@ -27,9 +27,6 @@ const uint16_t node_id = 00102;
 // Declare relays digital pins 
 #define RELAY_1  8
 #define RELAY_2  7
-// Declare relays state
-#define RELAY_ON  0
-#define RELAY_OFF  1
 
 // Declare pushbutton digital pin
 #define BUTTON  4
@@ -71,11 +68,11 @@ void setup(void)
   mesh.begin(channel, node_id);
   
   // initialize relays, turned off
-  digitalWrite(RELAY_1, RELAY_OFF);
-  digitalWrite(RELAY_2, RELAY_OFF);
+  //digitalWrite(RELAY_1, RELAY_OFF);
+  //digitalWrite(RELAY_2, RELAY_OFF);
   // initialize relays pins
-  pinMode(RELAY_1, OUTPUT);  
-  pinMode(RELAY_2, OUTPUT);
+  //pinMode(RELAY_1, OUTPUT);  
+  //pinMode(RELAY_2, OUTPUT);
 }
 
 //
@@ -108,17 +105,36 @@ void led(int pin) {
     case LED_RED:
       digitalWrite(LED_RED, HIGH);
       digitalWrite(LED_GREEN, LOW);
-      if(DEBUG) printf("LED: Info: RED led is enabled.");
+      if(DEBUG) printf("LED: Info: RED led is enabled.\n\r");
       return;
     case LED_GREEN:
       digitalWrite(LED_GREEN, HIGH);
       digitalWrite(LED_RED, LOW);
-      if(DEBUG) printf("LED: Info: GREEN led is enabled.");
+      if(DEBUG) printf("LED: Info: GREEN led is enabled.\n\r");
       return;
     default: 
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_RED, LOW);
-      if(DEBUG) printf("LED: Info: Leds off.");
+      if(DEBUG) printf("LED: Info: Leds turned off.\n\r");
+      return;
+  }
+}
+
+/****************************************************************************/
+
+void handle_button() {
+  
+  switch ( read_button() ) {
+    case 1:
+
+      return;
+    case 2:
+
+      return;
+    case 3:
+
+      return;
+    default:
       return;
   }
 }
@@ -133,45 +149,59 @@ int read_button() {
   if(digitalRead(BUTTON) == LOW) {
     return 0;
   }
-  // wait for checking
   delay(1000);
   if(digitalRead(BUTTON) == LOW) {
-    // skip if it pushed occasional
+    // skip if it pushed by mistake
+    printf("BUTTON: Error: Sticky button!\n\r");
     return 0;
   }
 
-  if(DEBUG) printf("BUTTON: Info: Button is pushed. And waiting for commands.");
+  if(DEBUG) printf("BUTTON: Info: Button is pushed. And waiting for commands.\n\r");
   // turned off leds
   led(LED_OFF);
 
   int count_pushed = 0;
   while( button_pushed() ) {
     count_pushed++;
-    if(DEBUG) printf("BUTTON: Info: Button is pushed: %d times", count_pushed);
+    if(DEBUG) printf("BUTTON: Info: Button is pushed: %d times.\n\r", count_pushed);
   }
   return count_pushed;
 }
 
 /****************************************************************************/
 
-  bool button_pushed() {
-    // wait for release the button
-    delay(500);
-    if(digitalRead(BUTTON) == HIGH) {
-      return false;
-    }
-    led(GREEN);
-    // wait for push the button
-    delay(500);
-    if(digitalRead(BUTTON) == HIGH) {
-      led(LED_OFF);
-      return true;
-    }
+bool button_pushed() {
+  // wait for release a button
+  delay(500);
+  if(digitalRead(BUTTON) == HIGH) {
+    return false;
   }
+  led(LED_GREEN);
+  // wait for push a button
+  delay(500);
+  if(digitalRead(BUTTON) == HIGH) {
+    led(LED_OFF);
+    return true;
+  }
+  return false;
+}
 
 /****************************************************************************/
 
-void relay()
+void relay_on(int pin) {
+  const int ON = 0;
+  // initialize relay pin
+  pinMode(pin, OUTPUT);
+
+  digitalWrite(pin, ON);
+
+  relay
+}
+
+void relay_off(int pin) {
+  const int OFF = 1;
+
+}
 
 /****************************************************************************/
 
@@ -182,12 +212,11 @@ bool read_DHT11(int& humidity, int& temperature) {
     case DHTLIB_OK:
       humidity = DHT11.humidity;
       temperature = DHT11.temperature;
-      if(DEBUG) printf("DH11: Info: Sensor values: humidity: %d, 
-                        temperature: %d.\n\r", humidity, temperature);
+      if(DEBUG) printf("DH11: Info: Sensor values: humidity: %d, temperature: %d.\n\r", 
+                          humidity, temperature);
       return true;
     case DHTLIB_ERROR_CHECKSUM:  
-      printf("DH11: Error: Checksum test failed!: 
-              The data was received but may be incorrect!\n\r");
+      printf("DH11: Error: Checksum test failed!: The data may be incorrect!\n\r");
       return false;
     case DHTLIB_ERROR_TIMEOUT: 
       printf("DH11: Error: Timeout occured!: Communication failed!\n\r");
@@ -201,6 +230,12 @@ bool read_DHT11(int& humidity, int& temperature) {
 /****************************************************************************/
 
 bool read_ACS712(float& amperage) {
+
+  //if( relays_off ) {
+    //calibrated_zero
+
+  //}
+
 ////  // turn on pullup resistors
 ////  pinMode(sensorPin, INPUT);
 ////  digitalWrite(sensorPin, HIGH);
