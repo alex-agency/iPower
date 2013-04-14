671092a7a17b8e7c4d66f5898130d78cbc92b138
Alex <alex.agency@gmail.com>
2013-04-14 22:25:09 +0300

    modified amperage calculate

--- acs712.h
+++ acs712.h
@@ -4,42 +4,40 @@
 #define ACSLIB_OK  0
 #define ACSLIB_DEBUG  true
 
-const int sensitivity = 200;
-uint16_t shift = 512;
+const int sensitivity = 100;
+uint16_t shift = 514;
 
 class acs712
 {
 public:
   int amperage;
-
+  
   int read(int pin) {
-    // initialize sensor pin with internal pullup resistor
-    pinMode(pin, INPUT_PULLUP);
-    // read sensor
-    uint16_t sensor = analogReadAccuracy(pin, sensitivity);
-    // shifting zero for calibrate
-    if(sensor > 509 && sensor < 515) {
-      shift = sensor;
-    }
-    // calculate
-    // 512 = 0A = 2500mV, shift = 512
-    // 100mV/(2500mV/512) = 20.48mA = 513
-    int delta = sensor - shift;
-    amperage = delta * 20.48;
-
-    if(ACSLIB_DEBUG) printf("ACS712: Info: Sensor: %u, Delta: %d, Amperage: %d.\n\r", 
-                        sensor, delta, amperage);
-    return ACSLIB_OK;
-  };
-
-private:
-  uint16_t analogReadAccuracy(int pin, int sensitivity) {
-    uint64_t sum = 0;
+    uint16_t sensor = 0;
+    uint16_t value = 0;
+    // initialize sensor pin
+    pinMode(pin, INPUT);
+    // get maximum value
     for(int i = 0; i < sensitivity; i++) {
-      sum = sum + analogRead(pin);
-      delay(5);
+      sensor = analogRead(pin);
+      if(sensor > value) {
+        value = sensor;
+      }
+      delay(50);
     }
-    return sum / sensitivity;
+    // shifting zero
+    if(value>=shift-2 && value<=shift) {
+      value = shift;
+    }
+    // calculate amperage
+    // 512 = 0A, 512*(5V/1024) = 2.5V
+    // 1A = 100mV, 10A = 1V = (1V/5V)*1024 = 204.8
+    // 1 = 10000mA/204.8 = 48,83 = 48mA
+    int delta = value - shift;
+    amperage = delta * 48;
+    if(ACSLIB_DEBUG) printf("ACS712: Info: Sensor: %u, Delta: %d, Amperage: %d, Power: %d.\n\r", 
+                        value, delta, amperage, (amperage*216)/1000);
+    return ACSLIB_OK;
   };
 };
 
