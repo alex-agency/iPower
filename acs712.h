f6b75614378f1dab5fef07ffaf8c602d988e1aa9
Alex <alex.agency@gmail.com>
2013-10-21 09:50:50 +0300

    fixed sensor

--- acs712.h
+++ acs712.h
@@ -4,8 +4,7 @@
 #define ACSLIB_OK  0
 #define ACSLIB_DEBUG  true
 
-const int sensitivity = 100;
-uint16_t shift = 514;
+const int sensitivity = 10;
 
 class acs712
 {
@@ -13,30 +12,32 @@ public:
   int amperage;
   
   int read(int pin) {
-    uint16_t sensor = 0;
-    uint16_t value = 0;
+    int sensor = 0;
+    int value = 0;
+    int delta = 0;
+    double sum = 0;
     // initialize sensor pin
     pinMode(pin, INPUT);
-    // get maximum value
-    for(int i = 0; i < sensitivity; i++) {
+    // get sum values
+    for(uint16_t i = 0; i < sensitivity; i++) {
       sensor = analogRead(pin);
-      if(sensor > value) {
-        value = sensor;
+      delta = sensor - 511;
+      if(delta < 0) {
+        delta = delta * (-1);
       }
-      delay(50);
+      sum = sum + delta;
+      delay(9);
     }
-    // shifting zero
-    if(value>=shift-2 && value<=shift) {
-      value = shift;
-    }
+    // calculate avarage value
+    value = sum / sensitivity;
     // calculate amperage
     // 512 = 0A, 512*(5V/1024) = 2.5V
     // 1A = 100mV, 10A = 1V = (1V/5V)*1024 = 204.8
-    // 1 = 10000mA/204.8 = 48,83 = 48mA
-    int delta = value - shift;
-    amperage = delta * 48;
-    if(ACSLIB_DEBUG) printf("ACS712: Info: Sensor: %u, Delta: %d, Amperage: %d, Power: %d.\n\r", 
-                        value, delta, amperage, (amperage*216)/1000);
+    // 1 = 10000mA/204.8 = 48,83 = 49mA
+    amperage = value * 49;
+    if(ACSLIB_DEBUG) printf("ACS712: Info: Delta: %d, Amperage: %d.\n\r", 
+                        value, amperage);
+    Serial.println((amperage * 216.0) / 1000.0);
     return ACSLIB_OK;
   };
 };
